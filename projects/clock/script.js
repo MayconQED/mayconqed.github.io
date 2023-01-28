e919b75bd1d8486d561fb13ee8ead775520498c2
primitiveofx <mayconmafra314@gmail.com>
2023-01-27 22:04:51 -0300

    六.6

--- projects/clock/script.js
+++ projects/clock/script.js
@@ -6,9 +6,9 @@ setInterval(function() {
     const hr = time.getHours()
     const min = time.getMinutes()
     const s = time.getSeconds()
-    const d = time.getUTCDate()
-    const m = (time.getUTCMonth() + 1)
-    const y = time.getUTCFullYear()
+    const d = time.getDate()
+    const m = (time.getMonth() + 1)
+    const y = time.getFullYear()
     clock.innerHTML = `${hr}:${min.toString().padStart(2, 0)}:${s.toString().padStart(2, 0)}`
     date.innerHTML = `${d.toString().padStart(2, 0)}日 ${m.toString().padStart(2, 0)}月 ${y.toString()}年`
     if(hr >= 5 && hr < 16){
