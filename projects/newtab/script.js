79052b9b99f9f1ef675477df0a5698ea3f47515d
primitiveofx <mayconmafra314@gmail.com>
2023-02-06 22:01:16 -0300

    六.12

---
+++ projects/newtab/script.js
@@ -0,0 +1,14 @@
+setInterval(function() 
+{
+    const clockdate = document.getElementById('clock-date')
+    const time = new Date()
+    const hr = time.getHours()
+    const min = time.getMinutes()
+    const s = time.getSeconds()
+    const d = time.getDate()
+    const m = (time.getMonth() + 1)
+    const y = time.getFullYear()
+    clockdate.innerHTML = `${hr}:${min.toString().padStart(2, 0)}:${s.toString().padStart(2, 0)} | ${d.toString().padStart(2, 0)}日 ${m.toString().padStart(2, 0)}月 ${y.toString()}年`
+})
+
+    
